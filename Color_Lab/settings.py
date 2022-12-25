d89402f2dcc8d3b732a3c2ca5e1a4d689464ab30
Alexey <aleksejdelov@gmail.com>
2022-12-25 11:56:46 +0300

    Переход на другой хостинг

--- Color_Lab/settings.py
+++ Color_Lab/settings.py
@@ -79,9 +79,11 @@ WSGI_APPLICATION = 'Color_Lab.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
